4903f22ad5c4f336764449862aee5ad0b4de0d5c
Katherine Whitlock <kate@skylinesynths.nyc>
2025-02-04 19:02:55 -0500

    Add test for both codepaths for GainRamp

--- tests/arm/spec/dsp/gain_ramp_spec.cpp
+++ tests/arm/spec/dsp/gain_ramp_spec.cpp
@@ -47,6 +47,16 @@ describe dsp_gain_ramp("Gain Ramp", $ {
 		gain_ramp.processBlock(in, out);
 		expect(out).to_equal(std::array{0.0f, 0.0625f, 0.125f, 0.1875f, 0.25f, 0.3125f, 0.375f, 0.4375f, 0.5f, 0.5625f, 0.625f, 0.6875f, 0.75f, 0.8125f, 0.875f, 0.9375f});
 	});
+
+	it("processes a large set of mono samples using NEON and non-NEON codepaths", _ {
+		blocks::GainRamp gain_ramp{0.0f, 1.0f};
+		std::array<float, 17> in;
+		in.fill(1.0f);
+		std::array<float, 17> out;
+		gain_ramp.processBlock(in, out);
+		expect(out).to_equal(std::array{0.0f, 0.0625f, 0.125f, 0.1875f, 0.25f, 0.3125f, 0.375f, 0.4375f, 0.5f, 0.5625f, 0.625f, 0.6875f, 0.75f, 0.8125f, 0.875f, 0.9375f, 1.0f});
+	});
+
 });
 
 CPPSPEC_SPEC(dsp_gain_ramp);
