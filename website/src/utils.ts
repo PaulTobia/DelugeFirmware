63f651d08857447c8be2e768ecb6215e872f2184
mgabor <9047995+mgabor3141@users.noreply.github.com>
2025-04-21 17:02:08 +0200

    Test PR checks

--- website/src/utils.ts
+++ website/src/utils.ts
@@ -1,5 +1,6 @@
 import config from "../astro.config.mjs"
 
-export const withBase = (path: string) => {
+export const withBase = (
+  path: string) => {
   return path.startsWith("/") ? `${config.base}${path}` : path
 }
